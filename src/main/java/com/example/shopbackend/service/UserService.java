dd0ec0c27d127594efbc4016e0f96413df5feeb5
SangJun92 <nice92x@naver.com>
2024-06-17 12:49:21 +0900

    security해제하고 user불러오기

--- src/main/java/com/example/shopbackend/service/UserService.java
+++ src/main/java/com/example/shopbackend/service/UserService.java
@@ -3,10 +3,21 @@ package com.example.shopbackend.service;
 import com.example.shopbackend.model.Role;
 import com.example.shopbackend.model.User;
 
+import java.util.List;
 import java.util.Optional;
 
+
 public interface UserService {
+
+    // 새로운 사용자를 저장합니다.
     User saveUser(User user);
+
+    // 사용자 이름으로 사용자를 찾습니다.
     Optional<User> findByUsername(String username);
+
+    // 사용자의 역할을 변경합니다
     void changeRole(Role newRole, String username);
+
+    // 모든 사용자를 조회합니다
+    List<User> userAll();
 }
--- src/main/java/com/example/shopbackend/service/UserServiceImpl.java
+++ src/main/java/com/example/shopbackend/service/UserServiceImpl.java
@@ -5,20 +5,20 @@ import com.example.shopbackend.model.User;
 import com.example.shopbackend.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
-public class UserServiceImpl implements UserService {
+public class UserServiceImpl implements UserService{
     private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
+//    private final PasswordEncoder passwordEncoder;
 
     public User saveUser(User user) {
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
+//        user.setPassword(passwordEncoder.encode(user.getPassword()));
         user.setRole(Role.USER);
         user.setCreateTime(LocalDateTime.now());
         return userRepository.save(user);
@@ -31,7 +31,12 @@ public class UserServiceImpl implements UserService {
 
     @Override
     @Transactional
-    public void changeRole(Role newRole, String username) {
+    public void changeRole(Role newRole, String username){
         userRepository.updateUserRole(username, newRole);
+    }
+
+    @Override
+    public List<User> userAll() {
+        return userRepository.findAll();
     }
 }
