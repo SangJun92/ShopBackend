496a7940a6062007a1edbf3e002a8c5e0d295b92
SangJun92 <nice92x@naver.com>
2024-06-28 12:22:19 +0900

    240628 ShopBackend

---
+++ src/main/java/com/example/shopbackend/security/jwt/JwtProvider.java
@@ -0,0 +1,14 @@
+package com.example.shopbackend.security.jwt;
+
+import com.example.shopbackend.security.UserPrinciple;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.core.Authentication;
+
+public interface JwtProvider {
+    String generateToken(UserPrinciple auth);
+
+    Authentication getAuthentication(HttpServletRequest request);
+
+    boolean isTokenValid(HttpServletRequest request);
+
+}
